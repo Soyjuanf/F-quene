6295854ea3c9ee84ebe5f3bc54b5d5bb7042155a
Soyjuanf <juanfmusic.26@gmail.com>
2025-05-28 16:29:01 -0500

    Update main.js

--- main.js
+++ main.js
@@ -11,25 +11,6 @@ document.addEventListener("DOMContentLoaded", () => {
     clipPath: "inset(0 0% 0 0)", // Revela horizontalmente
     ease: "none"
 
-  gsap.to(".capsule-image", {
-    scrollTrigger: {
-      trigger: ".capsule-transition-section",
-      start: "top top",
-      end: "bottom top",
-      scrub: true
-    },
-    scale: 1.8,
-    borderRadius: "0px"
-  });
-
-  gsap.to(".capsule-overlay", {
-    scrollTrigger: {
-      trigger: ".capsule-transition-section",
-      start: "center center",
-      end: "bottom top",
-      scrub: true
-    },
-    opacity: 1
   });
 });
 
