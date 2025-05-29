786ed953b026cb06d927943bb57f8a9ced7e8ef5
Soyjuanf <juanfmusic.26@gmail.com>
2025-05-29 10:38:59 -0500

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const images = [".image-1", ".image-2", ".image-3"];
   const texts = [".text-1", ".text-2", ".text-3"];
-  const durations = [300, 300, 200]; // Duración del scroll por imagen
+  const durations = [700, 700, 450]; // Duración del scroll por imagen
 
   images.forEach((img, i) => {
     const start = durations.slice(0, i).reduce((a, b) => a + b, 0);
@@ -22,14 +22,14 @@ document.addEventListener("DOMContentLoaded", () => {
     // Imagen 1 arranca pequeña
     if (i === 0) {
       gsap.set(img, { scale: 0.6 });
-      tl.to(img, { opacity: 1, scale: 1.3, duration: 1, ease: "power2.out" });
+      tl.to(img, { opacity: 1, scale: 1.3, duration: 3, ease: "power2.out" });
     } else {
-      tl.to(img, { opacity: 1, scale: 1.1, duration: 1, ease: "power2.out" });
+      tl.to(img, { opacity: 1, scale: 1.1, duration: 3, ease: "power2.out" });
     }
 
     // Texto aparece y desaparece dentro del mismo timeline que su imagen
-    tl.to(texts[i], { opacity: 1, duration: 0.5 }, "<+0.2");
-    tl.to(texts[i], { opacity: 0, duration: 0.5 }, `>-${0.5}`);
+    tl.to(texts[i], { opacity: 1, duration: 5 }, "<+0.2");
+    tl.to(texts[i], { opacity: 0, duration: 5 }, `>-${0.5}`);
   });
 
   // Z-index correcto
